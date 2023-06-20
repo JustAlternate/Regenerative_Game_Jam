7711fa7fab5c3c8045f8519eef286fb033b0b00c
JustAlternate <lololollololol890@gmail.com>
2023-06-20 11:47:17 +0200

    mettage des cara des plantes dans le code

---
+++ Scripts/plante.gd
@@ -0,0 +1,149 @@
+extends Node2D
+
+var state = 0 # 0=graine, 1=plante, -1=morte.
+
+@export var type_plant= "None" #"carrot", "pea", "leek", "corn", "wheat", "pumpkin", "tomatoes", "thym", "vine", "courgette"
+var bonus_season = [] # (0,1) = Summer1, (1,2) = spring2, (2,1) = winter1, (3,1) = autumn1
+var required_season = [] # (0,1) = Summer1, (1,2) = spring2, (2,1) = winter1, (3,1) = autumn1
+var number_of_phase:int
+var required_humidities_values = [] # 0 = sec, 1 = humide, 2 = très humide
+var minimum_nutriment_values = [] # 0 = pas de nutriment, 1 = un peu nutriment, 2 = tres nutriments
+var required_sunlight = [] # 0 = vulnerable to sun, 1 = dont care about sunlight, 2 = absolutly need sunlight
+var sickness = [] # "mildiou", "Oïdium", "Furariose"
+var appreciated_adjacents_plants = [] # "carrot", "pea", "leek", "corn", "wheat", "mint", "pumpkin", "tomatoes", "thym", "vine", "courgette"
+var unapreciated_adjacents_plants = [] # pareil que au dessus.
+
+var dico_caracteristique = {
+	"bonus_season":
+	{# (0,1) = Summer1, (1,2) = spring2, (2,1) = winter1, (3,1) = autumn1
+		"pea":[[2,1],[2,0]],
+		"leek":[[1,2],[0,1]],
+		"corn":[[1,1]],
+		"wheat":[[2,2]],
+		"carrot":[[1,1]],
+		"mint":[[1,1]],
+		"pumpkin":[[1,2]],
+		"tomatoes":[[1,1]],
+		"thym":[[3,1]],
+		"vine":[[2,1],[2,2]],
+		"courgette":[[1,1]],
+	},
+	"required_season":
+	{# (0,1] = Summer1, (1,2) = spring2, (2,1) = winter1, (3,1) = autumn1
+		"pea":[[1,1],[0,2]],
+		"leek":[[3,2],[0,2]],
+		"corn":[[1,2]],
+		"wheat":[[3,1]],
+		"carrot":[[2,2],[1,2]],
+		"mint":[[2,2],[1,2]],
+		"pumpkin":[[1,1],[0,1]],
+		"tomatoes":[[2,2],[1,2]],
+		"thym":[[3,1]],
+		"vine":[[3,2],[1,1]],
+		"courgette":[[2,2],[1,2]],
+	},
+	"number_of_phases":{
+		"pea":2,
+		"leek":3,
+		"corn":2,
+		"wheat":4,
+		"carrot":3,
+		"mint":3,
+		"pumpkin":3,
+		"tomatoes":2,
+		"thym":4,
+		"vine":4,
+		"courgette":2,
+	},
+	"required_humidities_values":{ # 0 = sec, 1 = humide, 2 = très humide
+		"pea":[1],
+		"leek":[0,1],
+		"corn":[2],
+		"wheat":[1],
+		"carrot":[1,2],
+		"mint":[2],
+		"pumpkin":[1,2],
+		"tomatoes":[1,2],
+		"thym":[0],
+		"vine":[1],
+		"courgette":[2],
+	},
+	"minimum_nutriment_values":{ # 0 = pas de nutriment, 1 = un peu nutriment, 2 = tres nutriments
+		"pea":0,
+		"leek":0,
+		"corn":1,
+		"wheat":0,
+		"carrot":2,
+		"mint":2,
+		"pumpkin":1,
+		"tomatoes":1,
+		"thym":0,
+		"vine":0,
+		"courgette":2,
+	},
+	"required_sunlight":{
+		"pea":0,
+		"leek":1,
+		"corn":2,
+		"wheat":2,
+		"carrot":1,
+		"mint":1,
+		"pumpkin":0,
+		"tomatoes":2,
+		"thym":2,
+		"vine":2,
+		"courgette":2,
+	},
+	"appreciated_adjacents_plants":{
+		"pea":[],
+		"leek":["carrot","tomatoes"],
+		"corn":["pumpkin"],
+		"wheat":[],
+		"carrot":["tomatoes","leek","pea"],
+		"mint":["thym","tomatoes","pea"],
+		"pumpkin":[],
+		"tomatoes":[],
+		"thym":["mint"],
+		"vine":[],
+		"courgette":["pea"],
+	},
+	"unapreciated_adjacents_plants":{
+		"pea":[],
+		"leek":["pea"],
+		"corn":[],
+		"wheat":[],
+		"carrot":[],
+		"mint":["carrot","corn"],
+		"pumpkin":[],
+		"tomatoes":[],
+		"thym":[],
+		"vine":[],
+		"courgette":[],
+	},
+	
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$sprite.animation = "0"
+	position.x = 0
+	position.y = 0
+	
+	if type_plant != "None" :
+		pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func next_state():
+	state += 1
+	if state == number_of_phase:
+		state = -1 #a changer en fonction de ce qui se passe lorsque la plante meurt.
+	$sprite.animation = str(state)  
+
+func dying():
+	state = -1
+	$sprite.animation = str(state)  #a changer en fonction de ce qui se passe lorsque la plante meurt.
+
