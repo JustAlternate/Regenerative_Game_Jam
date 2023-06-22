44c841b01346036d223b0b70b22d7c942fbf9bfa
nyoukki <gregory.miyazawa@gmail.com>
2023-06-22 10:17:27 +0200

    correction crash

--- Scripts/meteo.gd
+++ Scripts/meteo.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var Energy:float = 1
 @export var Canvas_modulate_color = Color8(0,0,0)
 @export var Sun_color = Color8(0,0,0)
-var sfx_tween = get_tree().create_tween()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,5 +15,4 @@ func _ready():
 func _process(delta):
 	pass
 
-func rain_fade_in():
-	$RainSFX.volume_db = -80
+
