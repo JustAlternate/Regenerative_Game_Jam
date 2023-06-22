a78c43ec8c1ccc8f114fb9591dca2fb7fad69464
nyoukki <gregory.miyazawa@gmail.com>
2023-06-22 18:49:00 +0200

    Musique spring 1 à l'écran titre

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$backgound/riviere.play("default")
+	$"/root/PersistentSfx/SpringMusic".play_song_phase1()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
