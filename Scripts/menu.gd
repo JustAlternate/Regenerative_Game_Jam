74a6f344ae06d2f536d0e875ed38cdab402f6e3d
JustAlternate <lololollololol890@gmail.com>
2023-06-22 18:05:21 +0200

    menu and home

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,13 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$riviere.play("default")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	for i in range ($Sprite_deroulant.get_child_count()):
 		if $Sprite_deroulant.get_child(i).position.x > 1000:
-			$Sprite_deroulant.get_child(i).position.x = -100
+			$Sprite_deroulant.get_child(i).position.x = -2200
 		$Sprite_deroulant.get_child(i).position.x +=1
 
 func _on_play_pressed():
