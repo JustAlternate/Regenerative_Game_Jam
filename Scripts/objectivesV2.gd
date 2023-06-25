d7652c1c7353502fa995b4999aef93b22e9df8ad
zazak <zazak.pascal@gmail.com>
2023-06-25 20:20:03 +0200

    game over réparation

--- Scripts/objectivesV2.gd
+++ Scripts/objectivesV2.gd
@@ -81,7 +81,7 @@ func next_season():
 		
 		compteur = 0
 		if ressource <= 0 or diversity <= 0 or taste <= 0:
-			get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
+			get_tree().change_scene_to_file("res://Scenes/game-over.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
